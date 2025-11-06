c9c4c9a233e97851ed7e698b090f40905bfdbf01
Wilson Solano <wilsonsolano96@gmail.com>
2025-11-06 05:11:11 -0500

    Configuración para Railway

--- PROYECTO YCW/backend/config/database.js
+++ PROYECTO YCW/backend/config/database.js
@@ -1,11 +1,11 @@
-const mysql = require('mysql2');
-
 const connection = mysql.createConnection({
-  host: process.env.MYSQLHOST || process.env.MYSQL_HOST || 'localhost',
-  user: process.env.MYSQLUSER || process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQLPASSWORD || process.env.MYSQL_PASSWORD || '',
-  database: process.env.MYSQLDATABASE || process.env.MYSQL_DATABASE || 'railway',
-  port: process.env.MYSQLPORT || process.env.MYSQL_PORT || 3306
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT
 });
 
+
 module.exports = connection;
+

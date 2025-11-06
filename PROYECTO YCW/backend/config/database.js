650d432eb2ea123e5bfd458bfca08c91580b0aee
Wilson Solano <wilsonsolano96@gmail.com>
2025-11-05 22:06:23 -0500

    fix: usar variable MYSQL de Railway

--- PROYECTO YCW/backend/config/database.js
+++ PROYECTO YCW/backend/config/database.js
@@ -1,11 +1,9 @@
 const mysql = require('mysql2');
 
-const connection = mysql.createConnection({
-    host: process.env.MYSQLHOST || 'localhost',
-    user: process.env.MYSQLUSER || 'root',
-    password: process.env.MYSQLPASSWORD || '',
-    database: process.env.MYSQLDATABASE || 'bd_arrendamientos',
-    port: process.env.MYSQLPORT || 3306
+const connection = mysql.createConnection(process.env.MYSQL || {
+  host: process.env.MYSQLHOST || 'localhost',
+  user: process.env.MYSQLUSER || 'root',
+  password: process.env.MYSQLPASSWORD || '',
+  database: process.env.MYSQLDATABASE || 'bd_arrendamientos',
+  port: process.env.MYSQLPORT || 3306
 });
-
-module.exports = connection;
